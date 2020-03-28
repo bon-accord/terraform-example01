32804712eba009e9ea341b32f2077e03676f22c2
Lewis Treacy <lewis.treacy@bp.com>
2020-03-28 16:44:14 +0000

    Add ELB

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # terraform-example01
+
+## Summary
+
+This is an example of a basic Terraform plan for AWS infrastructure.
+
+## AWS Components
+
+ - EC2 instance
+ - Security groups
+ - Auto-scaling Groups and related lauch configuration
+ - Elastic load-balancer
+
+## Relationships
+
+ - Security groups control traffic ingress & egress rules to EC2 instances and the ELB
+ - The launch configuration defines what the auto-scaling group should launch
+ - The elastic load-balancer defines a healthcheck to determine instance health
--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "example-resource" {
 
     user_data = <<-EOF
                 #!/bin/bash
-                echo "Hello "${var.hello_target}"'s World! on $(date "+%Y-%m-%d") @ $(date "+%H:%M:%S")" > index.html
+                echo "Hello "${var.hello_target}"'s World! from Host $(hostname) on Date $(date "+%Y-%m-%d") @ $(date "+%H:%M:%S") hrs" > index.html
                 nohup busybox httpd -f -p "${var.server_port}" &
                 EOF
 
@@ -35,6 +35,26 @@ resource "aws_security_group" "secgrp-instance" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_security_group" "elb" {
+    name = "terraform-example-elb"
+
+    # Allow all outbounfd
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    # Allow inbound HTTP from anywhere
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 
 # DEFINE VARIABLES
 
@@ -58,13 +78,14 @@ output "public_ip" {
 # DEFINE ASGs & LAUNCH CONFIGURATIONS
 
 resource "aws_launch_configuration" "example" {
+    name_prefix     = "lewis-launch-config"
     image_id        = "ami-0c216d3ab383cc403"    
     instance_type   = "t2.micro"
     security_groups = [aws_security_group.secgrp-instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
-                echo "Hello "${var.hello_target}"'s World! on $(date "+%Y-%m-%d") @ $(date "+%H:%M:%S")" > index.html
+                echo "Hello "${var.hello_target}"'s World! from Host $(hostname) on Date $(date "+%Y-%m-%d") @ $(date "+%H:%M:%S") hrs" > index.html
                 nohup busybox httpd -f -p "${var.server_port}" &
                 EOF
 
@@ -80,6 +101,9 @@ resource "aws_autoscaling_group" "example" {
   min_size = 2
   max_size = 10
 
+  load_balancers    = [aws_elb.example.name]
+  health_check_type = "ELB"
+
   tag {
       key                   = "Name"
       value                 = "terraform-asg-example"
@@ -92,5 +116,31 @@ resource "aws_autoscaling_group" "example" {
 data "aws_availability_zones" "all" {}
 
 # DEFINE LOAD-BALANCER (CLB TYPE)
+
+resource "aws_elb" "example" {
+  name                  = "terraform-asg-example"
+  security_groups       = [aws_security_group.elb.id]
+  availability_zones    = data.aws_availability_zones.all.names
+
+  health_check {
+      target                = "HTTP:${var.server_port}/"
+      interval              = 30
+      timeout               = 3
+      healthy_threshold     = 2
+      unhealthy_threshold   = 2
+  }
+
+  listener {
+      lb_port           = 80
+      lb_protocol       = "http"
+      instance_port     = var.server_port
+      instance_protocol = "http"
+  }
+}
+
+output "clb_dns_name" {
+  value         = aws_elb.example.dns_name
+  description   = "The domain of the load balancer"
+}
 
 
